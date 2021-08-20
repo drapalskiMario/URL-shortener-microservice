d78b42e4f3ca5cb84fe2e0b20a783d27597b4371
Mario Junior <drapalskimario@gmail.com>
2021-08-20 16:42:00 -0300

    added solution for URL Shortener Microservice

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# [URL Shortener Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/url-shortener-microservice)
+# URL Shortener Microservice
+
+This project is my solution for [APIs and Microservices Projects - URL Shortener Microservice] and it's a part of APIs and Microservices Certification by [freeCodeCamp].
+
+## Requirements
+
+✅ Sprovide your own project, not the example URL.
+
+✅ You can POST a URL to `/api/shorturl` and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+
+✅ When you visit `/api/shorturl/<short_url>`, you will be redirected to the original URL.
+
+✅ If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+## Installation
+
+### Install dependencies
+```bash
+$ npm i
+```
+### Start the server
+```bash
+$ npm start
+```
+
+[APIs and Microservices Projects - URL Shortener Microservice]: https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/url-shortener-microservice
+[freeCodeCamp]: https://www.freecodecamp.org/
+
+
+
+
--- server.js
+++ server.js
@@ -1,24 +1,62 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns')
+const mongoose = require('mongoose')
+const urlParser = require('url')
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose.connect('mongodb+srv://mariojunior:1234@cluster0.4oovf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true })
+
+const schema = new mongoose.Schema({ url: String })
+const Url = mongoose.model('Url', schema)
+
+app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body
+
+  dns.lookup(urlParser.parse(url).hostname, (err, address) => {
+    if (!address) {
+      res.json({ error: 'invalid url' })
+    } else {
+      const validUrl = new Url({ url })
+      validUrl.save((err, data) => {
+        res.json({
+          'original_url': data.url , 'short_url': data.id
+        })
+      })
+    }
+  })
 });
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const { id } = req.params
+  Url.findById(id, (err, data) => {
+    if (!data) {
+      res.json({ error: 'invalid url' })
+    } else {
+      res.redirect(data.url) 
+    }
+  })
+})
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+})
